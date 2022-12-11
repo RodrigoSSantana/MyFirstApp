6d8ea65c5b1c090ba4086e149ae3551a4e55a5e2
Rodrigo Santana <rodrigosantana0408@live.com>
2022-12-11 20:39:37 -0300

    adding a class called paint view and creating the buttons

--- app/src/main/java/com/example/paintapp/MainActivity.kt
+++ app/src/main/java/com/example/paintapp/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.paintapp
 
+import android.graphics.Paint
+import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        var Path = Path()
+        var paintBrush = Paint ()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val redBtn = findViewById<ImageButton>(R.id.redColor)
+        val blueBtn = findViewById<ImageButton>(R.id.blueColor)
+        val blackBtn = findViewById<ImageButton>(R.id.blackColor)
+        val eraserBtn = findViewById<ImageButton>(R.id.whiteColor)
+
+        redBtn.setOnClickListener{
+    Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
+        }
+        blueBtn.setOnClickListener{
+            Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
+        }
+        blackBtn.setOnClickListener{
+            Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
+        }
+        eraserBtn.setOnClickListener{
+            Toast.makeText(this,"Clicked",Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
